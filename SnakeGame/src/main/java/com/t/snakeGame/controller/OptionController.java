1616523d940559fa3cab91b614f552a2f3edf3bd
Martin-HZK <ManUnitedHZK7@outlook.com>
2023-11-18 15:27:02 +0800

    Create Color pane

--- SnakeGame/src/main/java/com/t/snakeGame/controller/OptionController.java
+++ SnakeGame/src/main/java/com/t/snakeGame/controller/OptionController.java
@@ -3,13 +3,17 @@ package com.t.snakeGame.controller;
 import javafx.event.ActionEvent;
 import com.t.snakeGame.Main;
 import javafx.fxml.FXML;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
 
 public class OptionController {
 
     @FXML
-//    private
+    private ColorPicker snakeColor = new ColorPicker(Color.GREEN);
+    @FXML
+    private ColorPicker textColor = new ColorPicker(Color.valueOf("#fc310d"));
 
     public void switchOnBackClick(ActionEvent actionEvent) throws IOException {
         Main.setRoot("/com.t.snakeGame/startMain");
