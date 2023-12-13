54c83d64b95dd99bb1e9767de917fa355f28157a
Martin-HZK <ManUnitedHZK7@outlook.com>
2023-12-14 02:39:46 +0800

    Help display the user history scores

--- SnakeGame/src/main/java/com/t/snakeGame/controller/ScoreController.java
+++ SnakeGame/src/main/java/com/t/snakeGame/controller/ScoreController.java
@@ -1,14 +1,28 @@
 package com.t.snakeGame.controller;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.t.snakeGame.Main;
+import com.t.snakeGame.model.Score;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ScoreController {
 
@@ -19,8 +33,23 @@ public class ScoreController {
     private Label totalScore;
     @FXML
     private Label playerLevel;
+
+    @FXML
+    private TableColumn<Score, String> playerName;
+
+    @FXML
+    private TableColumn<Score, String> playerScore;
+
+    @FXML
+    private TableView<Score> scoreTable;
+
     @FXML
     public void initialize() {
+        playerName.setCellValueFactory(new PropertyValueFactory<Score, String>("playerName"));
+        playerScore.setCellValueFactory(new PropertyValueFactory<Score, String>("playerScore"));
+
+        scoreTable.setItems(setScoreBoard());
+
         totalScore.setText(playingScore.get()+ "");
         try {
             if (playingScore.get() < 2) { // we set 5
@@ -44,9 +73,43 @@ public class ScoreController {
             e.printStackTrace();
         }
 
+
+
+        // load table
+
     }
 
     public void switchOnRestartClick() throws IOException {
         Main.setRoot("/com.t.snakeGame/startMain");
     }
+
+    public ObservableList<Score> setScoreBoard() {
+        JsonArray userScores = new JsonArray();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/Score.json"));
+            JsonObject jsonObject= JsonParser.parseReader(reader).getAsJsonObject();
+            reader.close();
+            userScores = jsonObject.getAsJsonArray("scores");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(userScores.size());
+        ArrayList<Score> scoreList = new ArrayList<>();
+        for (JsonElement scoreName : userScores) {
+            JsonObject userScore = scoreName.getAsJsonObject();
+            String name = userScore.get("name").getAsString();
+            String score = userScore.get("score").getAsString();
+            Score newScore = new Score(name, score);
+            scoreList.add(newScore);
+        }
+
+//        for (int i = 0; i < scoreList.size(); i++) {
+//            System.out.println(scoreList.get(i).getPlayerName() + ": " + scoreList.get(i).getPlayerScore());
+//
+//        }
+        return FXCollections.observableArrayList(scoreList);
+    }
+
+
+
 }
