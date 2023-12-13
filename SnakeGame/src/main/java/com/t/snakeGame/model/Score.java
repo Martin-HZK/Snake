25cf5b12703f0853070bffa6be2dc7baffc9c30f
Martin-HZK <ManUnitedHZK7@outlook.com>
2023-12-13 21:33:04 +0800

    Creating Score.java in model

---
+++ SnakeGame/src/main/java/com/t/snakeGame/model/Score.java
@@ -0,0 +1,31 @@
+package com.t.snakeGame.model;
+
+public class Score {
+    private String name;
+    private int score;
+
+    public Score(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+
+
+
+}
