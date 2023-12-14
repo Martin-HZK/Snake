1a1f250b04cae75a58bb544b8786bdf0d5c89815
Martin-HZK <ManUnitedHZK7@outlook.com>
2023-12-14 14:52:21 +0800

    Creating username input window

---
+++ SnakeGame/src/main/java/com/t/snakeGame/controller/UserNameController.java
@@ -0,0 +1,21 @@
+package com.t.snakeGame.controller;
+
+import com.t.snakeGame.Main;
+import com.t.snakeGame.model.Score;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class UserNameController {
+    @FXML
+    private TextField username;
+
+    Score playerScore;
+    public void switchOnPlayClick(ActionEvent actionEvent) throws IOException {
+        playerScore.setPlayerName(username.getText());
+        Main.setRoot("/com.t.snakeGame/view/playingView");
+    }
+}
